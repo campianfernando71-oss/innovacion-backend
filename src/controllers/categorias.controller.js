46361191f5973c6d141b74e87f6f7f4bb108c974
campianfernando71-oss <campianfernando71@gmail.com>
2025-12-01 21:03:34 +0000

    commit

---
+++ src/controllers/categorias.controller.js
@@ -0,0 +1,25 @@
+import { db } from "../config/db.js";
+
+export const getCategorias = async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM categorias");
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const createCategoria = async (req, res) => {
+  const { nombre } = req.body;
+
+  try {
+    const [result] = await db.query(
+      "INSERT INTO categorias (nombre) VALUES (?)",
+      [nombre]
+    );
+
+    res.json({ id: result.insertId, nombre });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
